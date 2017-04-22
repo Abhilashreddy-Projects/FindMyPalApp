60fc2a24ab26fcfd765b3d12e7871cc68b57b064
findmypal <findmypal10@gmail.com>
2017-04-21 22:33:18 -0400

    Add Login and User Registration Module

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,18 @@ from bottle import route, view, request, get, post
 from datetime import datetime
 from bottle import template
 
+
+@route('/')
+@route('/login')
+@view('login')
+def home():
+    """Renders the home page."""
+    return dict (
+    )
+
+
+@route('/register')
+@view('Register')
+def register():
+    """ returns register page """
+    return dict()
